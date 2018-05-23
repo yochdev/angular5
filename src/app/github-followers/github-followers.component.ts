955a7f1764805a1c71c5f080e088c542b28de0c1
Younes <younes@circl.link>
2018-05-23 14:35:24 +0200

    problem followers solved

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -8,14 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
+  /*data = {
+    login: '',
+    id: 0,
+    avatar_url: '',
+    gravatar_id: '',
+    url: '',
+    html_url: '',
+    followers_url: '',
+    following_url: '',
+    gists_url: '',
+    starred_url: '',
+    subscriptions_url: '',
+    organizations_url: '',
+    repos_url: '',
+    events_url: '',
+    received_events_url: '',
+    type: '',
+    site_admin: false
+  };*/
 
   constructor(private service: GithubFollowersService ) { }
 
   ngOnInit() {
+    this.getFollowers();
   }
 
   getFollowers() {
     this.service.getAll()
-    .subscribe(data => this.followers = data);
+    .subscribe(followers => this.followers = followers);
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,8 +2,6 @@ import { DataService } from './data.service';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-
-
 @Injectable()
 export class PostService extends DataService {
 
