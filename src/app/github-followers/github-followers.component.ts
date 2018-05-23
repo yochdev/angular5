4087b0fec8f22b39ead50e877c4e41d3f60f4d8a
Younes <younes@circl.link>
2018-05-23 15:08:35 +0200

    Follower modified it's work with the routerlink

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -8,25 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
-  /*data = {
-    login: '',
-    id: 0,
-    avatar_url: '',
-    gravatar_id: '',
-    url: '',
-    html_url: '',
-    followers_url: '',
-    following_url: '',
-    gists_url: '',
-    starred_url: '',
-    subscriptions_url: '',
-    organizations_url: '',
-    repos_url: '',
-    events_url: '',
-    received_events_url: '',
-    type: '',
-    site_admin: false
-  };*/
 
   constructor(private service: GithubFollowersService ) { }
 
@@ -36,6 +17,6 @@ export class GithubFollowersComponent implements OnInit {
 
   getFollowers() {
     this.service.getAll()
-    .subscribe(followers => this.followers = followers);
+    .subscribe(data => this.followers = data);
   }
 }
